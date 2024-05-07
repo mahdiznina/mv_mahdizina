d19ae77862add29d39f8c00a34372d486bab220c
mahdi znina <mahdiznina3@gmail.com>
2024-05-07 15:46:19 +0100

    Doctrine Extensions: Timestampable

--- src/Entity/VinylMix.php
+++ src/Entity/VinylMix.php
@@ -2,13 +2,16 @@
 
 namespace App\Entity;
 
-use App\Repository\VinylMixRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\VinylMixRepository;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: VinylMixRepository::class)]
 class VinylMix
 {
+    use TimestampableEntity;
+    
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -26,15 +29,9 @@ class VinylMix
     #[ORM\Column(length: 255)]
     private ?string $genre = null;
 
-    #[ORM\Column]
-    private \DateTimeImmutable $createdAt;
 
     #[ORM\Column]
     private int $votes = 0;
-    public function __construct()
-    {
-        $this->createdAt = new \DateTimeImmutable();
-    }
 
     public function getId(): ?int
     {
@@ -85,18 +82,6 @@ class VinylMix
     public function setGenre(string $genre): static
     {
         $this->genre = $genre;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeImmutable $createdAt): static
-    {
-        $this->createdAt = $createdAt;
 
         return $this;
     }
